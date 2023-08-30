4c4fd745d5a70f3f420982172a2f9be54ad871a5
junseop <jay000111@naver.com>
2023-08-30 19:25:43 +0900

    Feat: Two finger gesture has implemented.
    Zoom in / out the object(furniture) by pinch zoom.
    
    Resolves: #11
    Related to: #9

--- Assets/Scripts/Gestures.cs
+++ Assets/Scripts/Gestures.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
@@ -19,6 +20,13 @@ public class Gestures : MonoBehaviour
 
     private bool isButtonTouched = false;
     private bool buttonTouched = false;
+
+    private float initialDistance;
+    private Vector3 prevScale;
+    private Vector3 scaleUnit = new Vector3(0.05f, 0.05f, 0.05f);
+    private Vector3 minScale = new Vector3(0.1f, 0.1f, 0.1f);
+    private Vector3 maxScale = new Vector3(2f, 2f, 2f);
+    private float prevHeight;
 
     // Update is called once per frame
     void Update()
@@ -45,7 +53,14 @@ public class Gestures : MonoBehaviour
 
                 if (!ignoreTouchInput)
                 {
-                    if (Input.touchCount == 1 && (controllingStatus == string.Empty || controllingStatus == "moving"))
+                    if (Input.touchCount == 2 && (controllingStatus == string.Empty || controllingStatus == "zoom"))
+                    {
+                        controllingStatus = "zoom";
+                        if (prevHeight == 0)
+                            prevHeight = GetHeight(placeFurniture);
+                        PinchZoom(touches[0], touches[1]);
+                    }
+                    else if (Input.touchCount == 1 && (controllingStatus == string.Empty || controllingStatus == "moving"))
                     {
                         controllingStatus = "moving";
                         FurnitureLocationByOneFinger(touches[0]);
@@ -65,12 +80,22 @@ public class Gestures : MonoBehaviour
             try
             {
                 placeFurniture = furniturePool.GetChild(0).gameObject;
+                prevScale = new Vector3(1f, 1f, 1f);
+                prevHeight = 0f;
             }
             catch (Exception e)
             {
 
             }
         }
+    }
+
+    // Calculate object's height.
+    private float GetHeight(GameObject gameObject)
+    {
+        MeshFilter meshFilter = gameObject.GetComponentInChildren<MeshFilter>();
+        Bounds bounds = meshFilter.sharedMesh.bounds;
+        return bounds.size.y;
     }
 
     // Change Position of object(furniture) by user's one finger touch.
@@ -95,6 +120,38 @@ public class Gestures : MonoBehaviour
             {
                 var hitPose = hits[0].pose;
                 furniturePool.transform.position = new Vector3(hitPose.position.x, hitPose.position.y, hitPose.position.z);
+            }
+        }
+    }
+
+    // Pinchzoom to zoom/unzoom object(furniture).
+    private void PinchZoom(Touch touch1, Touch touch2)
+    {
+        if (Input.touchCount == 2)
+        {
+            float currentDistance = Vector2.Distance(touch1.position, touch2.position);
+            if (initialDistance == 0f)
+            {
+                initialDistance = currentDistance;
+
+            }
+            else
+            {
+                if (initialDistance > currentDistance)//축소
+                {
+                    if (minScale.x >= prevScale.x)
+                        return;
+                    furniturePool.transform.localScale = prevScale - scaleUnit;
+                }
+                else if (initialDistance < currentDistance)
+                {
+                    if (prevScale.x >= maxScale.x)
+                        return;
+                    furniturePool.transform.localScale = prevScale + scaleUnit;
+                }
+
+                prevScale = furniturePool.transform.localScale;
+
             }
         }
     }
