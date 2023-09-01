f47bdeada2a67d82462e830c6606b212d8893c65
junseop <jay000111@naver.com>
2023-09-01 17:39:51 +0900

    Feat: .obj Importer
    The .obj file obtained from an external path using objImporter is used by unity as a game object.
    
    Resolves: #15
    Related to: #14

--- Assets/Scripts/FurnitureCollocationButton.cs
+++ Assets/Scripts/FurnitureCollocationButton.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.XR.ARFoundation;
+using Dummiesman;
 
 public class FurnitureCollocationButton : MonoBehaviour
 {
@@ -23,8 +24,7 @@ public class FurnitureCollocationButton : MonoBehaviour
     // Start is called before the first frame update.
     void Awake()
     {
-        furniturePath = "Furniture/mesh";
-        loadFurniture = Resources.Load<GameObject>(furniturePath);
+        furniturePath = "/Users/junseop/Desktop/sdfasdf/a/mesh.obj";
         button.onClick.AddListener(CollocationButtonClickEvent);
     }
 
@@ -35,26 +35,29 @@ public class FurnitureCollocationButton : MonoBehaviour
 
         if (buttonText.text.Equals("Image_Input"))
         {
-            
             button.GetComponent<Image>().sprite = buttonImages[0];
             placementIndicator.SetActive(false);
-            GameObject placeFurniture = Instantiate(loadFurniture, placementIndicator.transform.position, placementIndicator.transform.rotation) as GameObject;
 
+            GameObject placeFurniture = new OBJLoader().Load(furniturePath);
+            placeFurniture.transform.position = placementIndicator.transform.position;
+            placeFurniture.transform.rotation = placementIndicator.transform.rotation;
 
-            //placeFurniture.GetComponent<Renderer>().material = furnitureMaterial;
             furniturePool.transform.position = placementIndicator.transform.position;
             placeFurniture.transform.SetParent(furniturePool);
             furniturePool.GetChild(0).GetChild(0).GetComponent<MeshRenderer>().material = furnitureMaterial;
             placeFurniture.transform.position = new Vector3(
                 placementIndicator.transform.position.x, placementIndicator.transform.position.y + (GetHeight(placeFurniture) / 2f), placementIndicator.transform.position.z);
+
             buttonText.SetText("Delete");
         }
         else if (buttonText.text.Equals("Delete"))
         {
             button.GetComponent<Image>().sprite = buttonImages[1];
             furniturePool.transform.localScale = new Vector3(1f, 1f, 1f);
+
             placementIndicator.SetActive(true);
             Destroy(furniturePool.GetChild(0).gameObject);
+
             buttonText.SetText("Image_Input");
             Destroy(instantiatedGesture);
         }
